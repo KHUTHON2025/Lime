17c086de21021306357d538321b684bf056a4711
shin0112 <wnslcosltimo12@gmail.com>
2025-05-09 21:34:55 +0900

    feat: 환경변수 shell 설정에서 .env 파일로 설정 변경하기

--- src/firebase.py
+++ src/firebase.py
@@ -1,8 +1,11 @@
 import os
 import firebase_admin
 from firebase_admin import credentials
+from dotenv import load_dotenv
 
-service_key_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+load_dotenv()
+
+service_key_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
 cred = credentials.Certificate(service_key_path)
 if not firebase_admin._apps:
